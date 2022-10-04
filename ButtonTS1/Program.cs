a95a42c070b0ed492e648db72a9ef287848a0c03
Rewben <dillon.tidwell@gmail.com>
2022-10-03 18:35:02 -0700

    Update 1.1

--- ButtonTS1/Program.cs
+++ ButtonTS1/Program.cs
@@ -6,32 +6,12 @@ Console.WriteLine("Start of Program.");
 static void Main(string[] args)
 {
     var order = new Order { Item = "Pizza with extra cheese" };
-            
     var orderingService = new FoodOrderingService();
-    var appService = new AppService();
-    var mailService = new MailService();
-
-    orderingService.FoodPrepared += appService.OnFoodPrepared;
-    orderingService.FoodPrepared += mailService.OnFoodPrepared;
-
     orderingService.PrepareOrder(order);
-    
     Console.ReadKey();
-}
-public class AppService
-{
-    public void OnFoodPrepared(object source, EventArgs eventArgs)
-    {
-        Console.WriteLine("NotificationService: your food is prepared");
-    }
-}
-
-public class MailService
-{
-    public void OnFoodPrepared(object source, EventArgs eventArgs)
-    {
-        Console.WriteLine("MailService: your food is prepared.");
-    }
+    Thread.Sleep(4000);
+    Console.WriteLine();
+    Thread.Sleep(4000);
 }
 public class Order
 {
@@ -42,40 +22,12 @@ public class Order
 
 public class FoodOrderingService
 {
-    // define a delegage
-    public delegate void FoodPreparedEventHandler(object source, EventArgs args);
-    // declare the event
-    public event FoodPreparedEventHandler FoodPrepared;
-
-    protected virtual void SendAppNotification(string Notify)
-    {
-        Console.WriteLine(Notify);
-    }
-    protected virtual void SendEmailNotification(string Notify)
-    {
-        Console.WriteLine(Notify);
-    }
     public void PrepareOrder(Order order)
     {
         Console.WriteLine($"Preparing your order '{order.Item}', please wait...");
         Thread.Sleep(4000);
-
-        OnFoodPrepared();
-
-        SendAppNotification("Notification Sent");
+        Console.WriteLine($"'{order.Item}' Complete.");
+        Thread.Sleep(4000);
     }
-    protected virtual void OnFoodPrepared()
-    {
-        if (FoodPrepared != null)
-            FoodPrepared(this, null);
-           
-        else
-            Console.WriteLine("FAILED to Prepare food.");
-    }
-
-    
-
-
 }
-
 
